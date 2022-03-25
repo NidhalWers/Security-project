cbb084bb8ddc4db3d720962315b7051ceb62a472
NidhalWers <nteyeb75@gmail.com>
2022-03-25 09:36:16 +0100

    delete

--- app-cyber/pages/load-users.php
+++
@@ -1,5 +0,0 @@
-<?php
-$offset = $argv[0]; // Attention, aucune validation!
-$query  = "SELECT id, name FROM products ORDER BY name LIMIT 20 OFFSET $offset;";
-$result = pg_query($conn, $query);
-?>
